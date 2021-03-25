ad67c361316c6552e9276a1fe58557fe3daa465c
alejandro <alegomezse@unal.edu.co>
2021-03-24 21:11:49 -0500

    ya corregido

--- r1/ProyectoRestadistica.R
+++ r1/ProyectoRestadistica.R
@@ -1,29 +1,31 @@
 #
 library (readxl)
 base <- datosEstadistica
-ceroGrados<- base$'20° s...7'
-veinteGrados<- base$'0° N...8'
-cuarentaGrados<- base$'40° N...9'
-sesentaGrados<- base$'60° N...10'
-ochentaGrados<- base$'80° N...11'
+ceroGrados <- base$'0° N...7'
+veinteGrados <- base$'20° N...8'
+cuarentaGrados <- base$'40° N...9'
+sesentaGrados <- base$'60° N...10'
+ochentaGrados <- base$'80° N...11'
 
-veinteGrados<- base$'0° s...8'
-ceroGrados<- base$'20° s...7'
-cuarentaGrados<- base$'40° s...9'
-sesentaGrados<- base$'60° N...10'
-ochentaGrados<- base$'80° N...11'
+veinteGradosS <- base$'0° S...6'
+cuarentaGradosS <- base$'40° S...5'
+sesentaGradosS <- base$'60° S...4'
+ochentaGradosS <- base$'80° S...3'
 
-
-
-
-
-
-ochentaGrados <- base$`80° S...3`
-
+hist(ceroGrados,breaks= 5)
+hist(veinteGrados,breaks = 5)
+hist(cuarentaGrados,breaks = 5)
+hist(sesentaGrados,breaks = 5)
 hist(ochentaGrados,breaks = 5)
 
-??tablar
+hist(veinteGradosS,breaks = 5)
+hist(cuarentaGradosS,breaks = 5)
+hist(sesentaGradosS,breaks = 5)
+hist(ochentaGradosS,breaks = 5)
 
+
+
+?hist
 
 
 
