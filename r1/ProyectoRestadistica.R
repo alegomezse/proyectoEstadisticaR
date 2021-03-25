1cade72ad8d14357f0ad58b9530e45d19b3729f9
alejandro <alegomezse@unal.edu.co>
2021-03-24 20:53:37 -0500

    ya

--- r1/ProyectoRestadistica.R
+++ r1/ProyectoRestadistica.R
@@ -1,8 +1,32 @@
 #
 library (readxl)
 base <- datosEstadistica
-hist(base$`80° S`)
-ochentaGrados
+veinteGrados<- base$'0° N...8'
+ceroGrados<- base$'20° s...7'
+cuarentaGrados<- base$'40° N...9'
+sesentaGrados<- base$'60° N...10'
+ochentaGrados<- base$'80° N...11'
+
+veinteGrados<- base$'0° N...8'
+ceroGrados<- base$'20° s...7'
+cuarentaGrados<- base$'40° N...9'
+sesentaGrados<- base$'60° N...10'
+ochentaGrados<- base$'80° N...11'
+
+
+
+
+
+
+ochentaGrados <- base$`80° S...3`
+
+hist(ochentaGrados,breaks = 5)
+
+??tablar
+
+
+
+
 
 
 
