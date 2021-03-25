dcec46636f955ba3f099dcdce85bb2f6161414e3
alejandro <alegomezse@unal.edu.co>
2021-03-24 21:01:48 -0500

    todo lindo

--- r1/ProyectoRestadistica.R
+++ r1/ProyectoRestadistica.R
@@ -1,15 +1,15 @@
 #
 library (readxl)
 base <- datosEstadistica
-veinteGrados<- base$'0° N...8'
 ceroGrados<- base$'20° s...7'
+veinteGrados<- base$'0° N...8'
 cuarentaGrados<- base$'40° N...9'
 sesentaGrados<- base$'60° N...10'
 ochentaGrados<- base$'80° N...11'
 
-veinteGrados<- base$'0° N...8'
+veinteGrados<- base$'0° s...8'
 ceroGrados<- base$'20° s...7'
-cuarentaGrados<- base$'40° N...9'
+cuarentaGrados<- base$'40° s...9'
 sesentaGrados<- base$'60° N...10'
 ochentaGrados<- base$'80° N...11'
 
